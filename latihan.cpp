d9b7e9294b0bb0658170833fea984645d7fd3171
Muhammad Faiz Bintang Pratama <143463503+Faizpy@users.noreply.github.com>
2023-09-02 11:02:33 +0700

    Hello
    alo

---
+++ latihan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x = 5;
+    int y = 2;
+    int z = x * y;
+
+    cout << "Hasil dari 5 dikali 2 adalah = " << z;
+
+
+
+
+
+ return 0;
+}
